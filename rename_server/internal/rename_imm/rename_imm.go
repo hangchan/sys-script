344b50fa38e9e2d82197f6880aade7445d5da608
Hang Chan <hang@needsystem.com>
2019-05-14 16:56:13 -0400

    saving progress

---
+++ rename_server/internal/rename_imm/rename_imm.go
@@ -0,0 +1,41 @@
+package rename_imm
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func RenameImm(oldIp string, newIp string, oldServer string, newServer string) {
+	// check if asu64 exists
+	cmd := "/opt/ibm/toolscenter/asu/asu64"
+	asu64 := make(map[string]string)
+	asu64 = map[string]string{
+		"IMM.HostIPAddress1":      "",
+		"IMM.DHCPAssignedHostIP1": "",
+		"IMM.IMMInfo_Name":        "",
+	}
+
+	if _, err := os.Stat(cmd); err == nil {
+
+		for k, _ := range asu64 {
+			show := cmd + " show " + k + " | tail -1"
+			showCmd := exec.Command("/bin/bash", "-c", show)
+			out, _ := showCmd.Output()
+			outArr := strings.Split(string(out), "=")
+			asu64[outArr[0]] = outArr[1]
+		}
+
+		if oldIp != asu64["IMM.HostIPAddress1"] || oldServer != asu64["IMM.IMMInfo_Name"] {
+			fmt.Println("Old Hostname or IP Address doesn't match, exiting")
+		} else {
+			fmt.Println(cmd, "set", "IMM.HostIPAddress1", newIp)
+			fmt.Println(cmd, "set", "IMM.DHCPAssignedHostIP1", newIp)
+			fmt.Println(cmd, "set", "IMM.IMMInfo_Name", newServer)
+		}
+
+	} else {
+		fmt.Println("asu64 util does not exist!")
+	}
+}
--- rename_server/rename_server.go
+++ rename_server/rename_server.go
@@ -1,28 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	co "github.com/hangchan/sys-script/rename_server/internal/rename_cobbler"
-	ic "github.com/hangchan/sys-script/rename_server/internal/rename_icinga"
-	rt "github.com/hangchan/sys-script/rename_server/internal/rename_racktable"
+	im "github.com/hangchan/sys-script/rename_server/internal/rename_imm"
 )
 
 var (
-	oldServer string = "old101.example.com"
-	newServer string = "new101.example.com"
+	oldServer = flag.String("oldServer", "old101", "the old server name")
+	newServer = flag.String("newServer", "new101", "the new server name")
+	oldIp     = flag.String("oldIp", "192.168.127.101", "the old ip address")
+	newIp     = flag.String("newIp", "172.16.127.101", "the new ip address")
 )
 
 func main() {
-	fmt.Println("test")
-	rt.RenameRacktable(oldServer, newServer)
-	fmt.Println(ic.RenameIcinga(oldServer, newServer))
-	co.RenameCobbler(oldServer, newServer)
-}
+	flag.Parse()
 
-// Get old and new server name for arg
-func getArg(oldServer string, newServer string) string {
-	// fmt.Printf("old:", oldServer, "new:", newServer)
-	return newServer
+	//rt.RenameRacktable(oldServer, newServer)
+	//fmt.Println(ic.RenameIcinga(oldServer, newServer))
+	//co.RenameCobbler(oldServer, newServer)
+	im.RenameImm(&oldServer, &newServer, &oldIp, &newIp)
 }
 
 // Replace entries in racktable
