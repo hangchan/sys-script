8dfd645e951d49620d8c8e2664eb3e2f2a8ac3f4
Hang Chan <hang@needsystem.com>
2019-05-15 11:51:03 -0400

    rename imm working

--- rename_server/rename_server.go
+++ rename_server/rename_server.go
@@ -2,24 +2,27 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	im "github.com/hangchan/sys-script/rename_server/internal/rename_imm"
 )
 
 var (
-	oldServer = flag.String("oldServer", "old101", "the old server name")
-	newServer = flag.String("newServer", "new101", "the new server name")
-	oldIp     = flag.String("oldIp", "192.168.127.101", "the old ip address")
-	newIp     = flag.String("newIp", "172.16.127.101", "the new ip address")
+	oldServer string
+	newServer string
+	oldIp     string
+	newIp     string
 )
 
 func main() {
+	flag.StringVar(&oldServer, "oldServer", "old101", "the old server name")
+	flag.StringVar(&newServer, "newServer", "new101", "the new server name")
+	flag.StringVar(&oldIp, "oldIp", "192.168.127.101", "the old ip address")
+	flag.StringVar(&newIp, "newIp", "172.16.127.101", "the new ip address")
 	flag.Parse()
 
 	//rt.RenameRacktable(oldServer, newServer)
 	//fmt.Println(ic.RenameIcinga(oldServer, newServer))
 	//co.RenameCobbler(oldServer, newServer)
-	im.RenameImm(&oldServer, &newServer, &oldIp, &newIp)
+	im.RenameImm(oldServer, newServer, oldIp, newIp)
 }
 
 // Replace entries in racktable
